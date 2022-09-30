eacc0c34f72455517735c1739ef45976f1466f81
Jaroslaw Charchula <charchula@gmail.com>
2022-09-30 02:43:24 +0200

    edit Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Assortment from '../components/Assortment';
+import Hero from '../components/Hero';
+import ServiceScope from '../components/ServiceScope';
+import SliderTypewriter from '../components/SliderTypewriter';
+import Contact from '../components/Contact';
+import GoogleMaps from '../components/GoogleMaps';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Hero />
+      <ServiceScope />
+      <SliderTypewriter />
+      <Assortment />
+      <Contact />
+      <GoogleMaps />
+    </main>
+  );
 };
 
 export default Home;
