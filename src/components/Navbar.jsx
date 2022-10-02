e8b43046223605b40ca03fb39c0f17cb4479e84d
Jaroslaw Charchula <charchula@gmail.com>
2022-10-02 02:56:38 +0200

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { linkClasses } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { languagePL, languageEN } from '../features/language/languageSlice';
@@ -20,9 +21,11 @@ const Navbar = () => {
     }
   };
 
+  // TODO navbar Animation, router links, scroll library
+
   return (
     <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 '>
-      <div class='container flex flex-wrap justify-between items-center mx-auto'>
+      <div class='relative container flex flex-wrap justify-between items-center mx-auto'>
         <a class='flex items-center'>asdadasd</a>
         <div class='flex items-center md:order-2'>
           <label for='toggle' class='flex items-center cursor-pointer'>
@@ -35,7 +38,7 @@ const Navbar = () => {
                 onChange={handleLanguage}
               />
               <div class='block bg-gray-500 w-14 h-8 rounded-full'></div>
-              <div class='flex justify-center items-center dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition text-sm'>
+              <div class='flex justify-center items-center dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition delay-150 text-sm'>
                 {isEnglish ? 'EN' : 'PL'}
               </div>
             </div>
@@ -63,7 +66,7 @@ const Navbar = () => {
         <div
           class={`${
             showNav ? '' : 'hidden'
-          } justify-between items-center w-full md:flex md:w-auto md:order-1`}
+          } justify-between items-center  md:flex md:w-auto md:order-1 md:static absolute top-10 -right-1 transition delay-300`}
         >
           <ul class='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
