fd8f1c63e27f5a52db26239347e1acd23e6626a7
Jaroslaw Charchula <charchula@gmail.com>
2022-10-26 23:35:15 +0200

    add partial styling

--- src/pages/SingleTypwriter/GalleryModal.jsx
+++ src/pages/SingleTypwriter/GalleryModal.jsx
@@ -15,19 +15,26 @@ const GalleryModal = ({
   };
 
   return (
-    <div className='dismiss' onClick={handleClick}>
-      <img src={clickedImg} alt='typewriter' />
-      <span className='cursor-pointer text-4xl' onClick={handleClick}>
+    <div
+      className='dismiss fixed top-0 left-0 right-0 w-full h-full flex items-center bg-black bg-opacity-50'
+      onClick={handleClick}
+    >
+      <img
+        className='block rounded-xl w-[30rem] h-[30rem] mx-auto my-16'
+        src={clickedImg}
+        alt='typewriter'
+      ></img>
+      <span className='cursor-pointer text-4xl z-40' onClick={handleClick}>
         <RiCloseCircleLine className='dismiss' />
       </span>
       <button
-        className='sliderTypewriter__btns  w-20 text-6xl mr-1 z-10'
+        className='sliderTypewriter__btns  w-20 text-6xl mr-1 z-40'
         onClick={handelRotationRight}
       >
         <BsChevronRight />
       </button>
       <button
-        className='sliderTypewriter__btns  w-20 text-6xl mr-1 z-10'
+        className='sliderTypewriter__btns  w-20 text-6xl mr-1 z-40'
         onClick={handelRotationLeft}
       >
         <BsChevronLeft />
