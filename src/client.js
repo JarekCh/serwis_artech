7c37f7550db1bea5a3555dd45ed04499bb3a4e6e
Jaroslaw Charchula <charchula@gmail.com>
2022-12-22 04:34:36 +0100

    remove sanity client add early version of PrivacyPolicy

--- src/client.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: 'v1',
-  useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
-});
